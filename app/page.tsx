a842ef15ca2473b5e52fc8e730c56e3a6733b911
alexlunster <alexlunster@gmail.com>
2025-08-29 20:44:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import AuthGate from '@/components/AuthGate';
-import UploadCard from '@/components/UploadCard';
-import TotalsBar from '@/components/TotalsBar';
-import HomeHint from '@/components/home/HomeHint';
+import AuthGate from "@/components/AuthGate";
 
 export default function Home() {
-  return (
-    <AuthGate>
-      <div className="grid gap-4">
-        <TotalsBar />
-        <UploadCard />
-        <HomeHint />
-      </div>
-    </AuthGate>
-  );
+  return <AuthGate />;
 }
